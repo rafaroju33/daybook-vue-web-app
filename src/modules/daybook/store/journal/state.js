4289482d621bc18a532abd0c4b47d18181cfd44a
Rafael <rafael@Rafaels-MacBook-Air.local>
2023-11-03 10:58:46 -0600

    Breakpoint options API

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   idLoading: true,
-  entries: [1, 2, 3, 4, 5, 6],
+  entries: [
+    {
+      id: new Date().getTime(), //1699024500501
+      date: new Date().toDateString(), //"Fri Nov 03 2023"
+      text: "Do ut esse sint Lorem et veniam laboris laboris aute ad irure mollit.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000, //1699024500501
+      date: new Date().toDateString(), //"Fri Nov 03 2023"
+      text: "Dolor esse aliqua ipsum tempor non cillum dolor excepteur dolor velit.",
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000, //1699024500501
+      date: new Date().toDateString(), //"Fri Nov 03 2023"
+      text: "Ex mollit non ut deserunt quis sunt excepteur laborum exercitation occaecat.",
+      picture: null,
+    },
+  ],
 });
